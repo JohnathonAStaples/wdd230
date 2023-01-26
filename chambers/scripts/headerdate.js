571a44122dfb03f962ced1f04c18e68c9c8c7666
JohnathonAStaples <staplesjohnathona@gmail.com>
2023-01-26 12:29:33 -0700

    week 4

---
+++ chambers/scripts/headerdate.js
@@ -0,0 +1,11 @@
+const datefieldUK = document.querySelector("aside"); 
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
