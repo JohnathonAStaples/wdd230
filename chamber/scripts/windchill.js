4316579f2c5f3d5c50a8b395a3ebb73d4efad169
johnathonastaples <staplesjohnathona@gmail.com>
2023-06-30 13:19:28 -0600

    big push for week 10

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,13 @@
  function windChillCalculator() {
-    var temperature = document.getElementById("temperature");
-    var windSpeed = document.getElementById("windSpeed");
-    var chill = document.getElementById("windChill");
+    const temperature = document.getElementById("temperature");
+    const windSpeed = document.getElementById("windSpeed");
+    const chill = document.getElementById("windChill");
 
-    var temp = parseFloat(temperature.textContent);
-    var wind = parseFloat(windSpeed.textContent);
+    const temp = parseFloat(temperature.textContent);
+    const wind = parseFloat(windSpeed.textContent);
 
     if (temp <= 50 && wind >= 3.0) {
-        var windChill = 
+        const windChill = 
         35.74+0.6214*temp-35.75*Math.pow(wind, 0.16)+0.4275*temp*Math.pow(wind, 0.16);
     chill.textContent = windChill.toFixed(2)
     } else {
@@ -16,3 +16,25 @@
  }
 
  windChillCalculator();
+ 
+
+ let weather = {
+    lat: "138.878445",
+    lon: "-104.869789",
+    apiKey:"ba11da35058f6814952f21a7b7a6a754",
+    fetchWeather: function (lat, lon) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + this.lat + "&lon=" + this.lon + "&units=imperial&appid=" + this.apiKey).then((response) => response.json())
+        .then((data) => console.log(data));
+       
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const {icon, description } = data.weather[0];
+        const {temp, feels_like} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, feels_like, speed);
+        document.querySelector(".city").innerHTML = "Weather in Garden of the Gods";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+    }
+ };
