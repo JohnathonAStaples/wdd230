309901e4e97a647c1792a414a4e282ed2f8ae83e
johnathonastaples <staplesjohnathona@gmail.com>
2023-06-30 14:46:34 -0600

    week 10

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,40 +1,74 @@
- /*function windChillCalculator() {
-    const temperature = document.getElementById("temperature");
-    const windSpeed = document.getElementById("windSpeed");
-    const chill = document.getElementById("windChill");
+// Select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("#description");
+const windSpeed = document.querySelector("#windSpeed");
 
-    const temp = parseFloat(temperature.textContent);
-    const wind = parseFloat(windSpeed.textContent);
-
-    if (temp <= 50 && wind >= 3.0) {
-        const windChill = 
-        35.74+0.6214*temp-35.75*Math.pow(wind, 0.16)+0.4275*temp*Math.pow(wind, 0.16);
-    chill.textContent = windChill.toFixed(2)
-    } else {
-        chill.textContent = "N/A";
-    }
- }
-
- windChillCalculator();*/
- 
-
- const url =
+const url =
  "https://api.openweathermap.org/data/2.5/weather?lat=38.8784&lon=-104.8697&units=imperial&appid=ba11da35058f6814952f21a7b7a6a754";
 
-let weather = {
-  
-  
-  fetchWeather: function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat=38.8784&lon=-104.8697&units=imperial&appid=ba11da35058f6814952f21a7b7a6a754" 
-    ).then((response) => response.json())
-    .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function(data){
-    const { name } = data;
-    const {icon, description} =data.weather[0];
-    const {temp, humidity} =data.main;
-    const {speed} = data.wind;
-    console.log (name,icon,description,temp,humidity, speed)
-    document.querySelector(".icon").src = "https://openweathermap.org/img.wn/" + icon + "@2x.png"
+ async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
+
+apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>°F`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = capitalizeFirstLetter(desc);
+
+  windSpeed.textContent = weatherData.wind.speed.toFixed(0);
+}
+
+function capitalizeFirstLetter(str) {
+  return str.replace(/^(.)|\s+(.)/g, function (match) {
+    return match.toUpperCase();
+  });
+}
+
+function calculateWindChill() {
+  var temperatureElement = document.querySelector("#current-temp");
+  var windSpeedElement = document.querySelector("#windSpeed");
+  var windChillElement = document.querySelector("#windChill");
+
+  // Extract the numerical values from the temperature and wind speed
+  var temperature = parseFloat(temperatureElement.textContent);
+  var windSpeed = parseFloat(windSpeedElement.textContent);
+
+  if (temperature <= 50 && windSpeed > 3.0) {
+    // Calculate the wind chill using the provided formula
+    var windChill =
+      35.74 +
+      0.6215 * temperature -
+      35.75 * Math.pow(windSpeed, 0.16) +
+      0.4275 * temperature * Math.pow(windSpeed, 0.16);
+
+    // Update the wind chill element with the calculated value
+    windChillElement.textContent = windChill.toFixed(2) + " °F";
+  } else {
+    windChillElement.textContent = "N/A";
+  }
+}
+
+calculateWindChill();
+
+const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
--- weather-api/scripts/weatherapi.js
+++ weather-api/scripts/weatherapi.js
@@ -30,5 +30,7 @@ function displayResults(weatherData) {
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute("alt", desc);
     captionDesc.textContent = desc;
+
+    
 }
 
