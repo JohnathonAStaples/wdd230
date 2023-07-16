e994af1da63328743794333be23ff53c7a333482
johnathonastaples <staplesjohnathona@gmail.com>
2023-07-16 08:07:13 -0600

    small nav screen

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -1,5 +1,14 @@
-// copyright and last modified
+// Button
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+  }
+  
+  const x = document.getElementById("hamburgerBtn");
+  x.onclick = toggleMenu;
+// Weather
 
+// copyright and last modified
 const today = new Date();
 const year = today.getFullYear();
 const msg = `&copy;  ${year}  Bountiful Foods | Johnathon A. Staples | California<br/>
