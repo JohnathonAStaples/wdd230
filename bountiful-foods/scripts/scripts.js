88bfba41c82739d43b19088a4ccd0f586912226b
johnathonastaples <staplesjohnathona@gmail.com>
2023-07-17 12:37:12 -0600

    heading css complete

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -8,7 +8,9 @@ function toggleMenu() {
   x.onclick = toggleMenu;
 // Weather
 
-// copyright and last modified
+const url = "https://api.openweathermap.org/data/2.5/forecast?lat=33.158092&lon=-117.350594&units=imperial&appid=ba11da35058f6814952f21a7b7a6a754";
+
+ // copyright and last modified
 const today = new Date();
 const year = today.getFullYear();
 const msg = `&copy;  ${year}  Bountiful Foods | Johnathon A. Staples | California<br/>
